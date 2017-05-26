a5254d9746c4337fbc048830ac2f73a50b8aca0b
butzsch <butzschered@gmail.com>
2017-05-26 10:34:35 +0200

    fix for previous test (...)

--- tests/narrowed_movement.cxx
+++ tests/narrowed_movement.cxx
@@ -12,7 +12,7 @@ TEST(UnlimitedMovement, IsntNarrowed)
 {
     static auto constexpr size = Vector { 1.0, 1.0 };
     static auto constexpr start = Rectangle { Vector { 0.0, 0.0 }, size };
-    static auto constexpr obstacle = Rectangle { Vector { 1.0, 1.0 }, size };
+    static auto constexpr obstacle = Rectangle { Vector { 10.0, 10.0 }, size };
     static auto constexpr delta_position = Vector { 1.0, 1.0 };
 
     ASSERT_THAT(get_narrowed_movement(start, delta_position, obstacle), Eq(delta_position));
